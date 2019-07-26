c14325bfb1cc2e132bcd940b310c6cfb9abf4152
Shakihassan <53328010+Shakihassan@users.noreply.github.com>
2019-07-26 10:59:00 +0530

    Add files via upload
    just commot

---
+++ sampleLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleLogin
+//
+//  Created by hassan on 30/05/19.
+//  Copyright © 2019 hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
